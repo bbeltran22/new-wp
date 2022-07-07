e356ec19ff127b7774027f3c6980502178fa0709
bbeltran22 <99772609+bbeltran22@users.noreply.github.com>
2022-07-07 09:11:52 -0600

    index and app js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather app</div>;
+export default function App() {
+  return (
+    <div className="App container mt-2">
+      <Weather defaultCity="New York" />
+    </div>
+  );
 }
-
-export default App;
